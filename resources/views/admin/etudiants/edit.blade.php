3fde573001521a97fb03118291980171d50b4a6e
jiiiiiiiiiiiiiiiiiiiiiiii <110284532+jiiiiiiiiiiiiiiiiiiiiiiii@users.noreply.github.com>
2023-06-12 14:04:24 +0100

    etudiant

--- resources/views/admin/etudiants/edit.blade.php
+++ resources/views/admin/etudiants/edit.blade.php
@@ -14,45 +14,44 @@
                 </div>
                 <div class="">
                     <form>
-                        <div class="w-full flex justify-between items-center">
-                            <div class="mr-2 w-full">
-                                <label for="firstname" class="block ml-1 text-md font-medium text-gray-700 mb-1">
+                        <div class="w-full flex justify-between items-center mt-4">
+                            <div class="mr-2 w-full ml-1">
+                                <label for="firstname" class="blocktext-md font-medium text-gray-700 mb-1">
                                     Prénom
                                 </label>
                                 <div class="mt-1">
-                                    <input type="text" name="firstname"
-                                        class="block focus:ring-4 w-full px-3 py-[10px] border border-gray1 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-gray2 focus:border-1 focus:border-gray2 sm:text-sm" />
+                                    Ahmed
                                 </div>
                             </div>
-                            <div class="w-full">
-                                <label for="lastname" class="block ml-1 text-md font-medium text-gray-700 mb-1">
+                            <div class="w-full ml-1">
+                                <label for="lastname" class="block text-md font-medium text-gray-700 mb-1">
                                     Nom
                                 </label>
                                 <div class="mt-1">
-                                    <input type="text" name="lastname"
-                                        class="block focus:ring-4 w-full px-3 py-[10px] border border-gray1 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-gray2 focus:border-1 focus:border-gray2 sm:text-sm" />
+                                    Simo
                                 </div>
                             </div>
                         </div>
-                        <div class="mt-2">
-                            <label for="email" class="block ml-1 text-md font-medium text-gray-700 mb-1">
-                                Email
-                            </label>
-                            <div class="mt-1">
-                                <input type="text" name="email"
-                                    class="block focus:ring-4 w-full px-3 py-[10px] border border-gray1 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-gray2 focus:border-1 focus:border-gray2 sm:text-sm" />
+                        <div class="w-full flex justify-between items-center mt-4">
+                            <div class="mr-2 w-full ml-1 ">
+                                <label for="tel" class="block text-md font-medium text-gray-700 mb-1">
+                                    Téléphone
+                                </label>
+                                <div class="mt-1">
+                                    +212 612345678
+                                </div>
                             </div>
-                        </div>
-                        <div class="mt-2">
-                            <label for="tel" class="block ml-1 text-md font-medium text-gray-700 mb-1">
-                                Téléphone
-                            </label>
-                            <div class="mt-1">
-                                <input type="text" name="tel"
-                                    class="block focus:ring-4 w-full px-3 py-[10px] border border-gray1 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-gray2 focus:border-1 focus:border-gray2 sm:text-sm" />
+                            <div class="w-full ml-1">
+                                <label for="email" class="block text-md font-medium text-gray-700 mb-1">
+                                    Email
+                                </label>
+                                <div class="mt-1">
+                                    ahmed@gmail.com
+                                </div>
                             </div>
+
                         </div>
-                        <div class="mt-2">
+                        <div class="mt-4">
                           <label for="niveau" class="block ml-1 text-md font-medium text-gray-700 mb-1">
                               Niveau
                           </label>
