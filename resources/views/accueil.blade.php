b772bacf7ae31bee51b91b1696a39f006f18c8eb
jiiiiiiiiiiiiiiiiiiiiiiii <110284532+jiiiiiiiiiiiiiiiiiiiiiiii@users.noreply.github.com>
2023-06-13 12:47:54 +0100

    acueil

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -35,82 +35,51 @@
         </div>
 
     </div>
-    <div class="px-12 flex items-start justify-between py-16">
-        <div class="w-[50%]">
-            <div class="text-black1 font-extrabold text-3xl">Pourquoi Megadev ?</div>
-            <div class="mt-4 text-lg text-black1 text-justify leading-6">
+    <div class="px-12 py-16">
 
-                Notre entreprise de développement est votre partenaire idéal pour propulser votre carrière vers de nouveaux
-                sommets. En choisissant notre centre de formation, vous bénéficiez d'une expertise inégalée dans un large
-                éventail de domaines. Que vous souhaitiez développer vos compétences en informatique, maîtriser des langues
-                étrangères, gérer des projets complexes ou explorer d'autres secteurs passionnants, nous avons les
-                formations spécialisées adaptées à vos besoins.
-
-                Ce qui nous distingue, c'est notre approche axée sur l'apprentissage pratique. Nous croyons fermement que la
-                meilleure façon d'acquérir des compétences durables est de les mettre en pratique. Nos formateurs
-                expérimentés vous guideront tout au long de votre parcours, en vous offrant des exercices pratiques et des
-                projets concrets pour renforcer votre compréhension et votre expertise. Vous apprendrez en faisant, en
-                développant une maîtrise réelle des compétences nécessaires pour exceller dans votre domaine.
-
-                De plus, notre environnement d'apprentissage stimulant favorise l'exploration de nouveaux horizons et la
-                prise de risques calculés. Nous encourageons nos apprenants à relever des défis, à sortir de leur zone de
-                confort et à repousser leurs limites. Vous serez entouré d'une communauté dynamique de professionnels
-                passionnés, tous animés par le désir de progresser et de réaliser leurs objectifs professionnels.
-
-                Rejoindre notre entreprise de développement, c'est faire le choix de la confiance en soi. Nous croyons en
-                votre potentiel et nous nous engageons à vous fournir les outils et les connaissances nécessaires pour
-                atteindre vos objectifs. Nos programmes de formation sont conçus sur mesure pour répondre à vos besoins
-                spécifiques, en vous offrant une expérience d'apprentissage personnalisée et enrichissante.
-
-                Ne tardez pas à nous rejoindre. Faites le premier pas dès aujourd'hui et laissez-nous vous accompagner sur
-                le chemin de l'excellence professionnelle.
-            </div>
-        </div>
-        <div class="w-[45%] grid grid-cols-2 md:grid-cols-2 gap-4 h-80">
-            <div class="grid">
-                <div class="max-w-full rounded-lg border-2 border-gray2 p-2 h-[440px] shadow-md">
+        <div class="text-black1 font-extrabold text-3xl">Pourquoi Megadev ?</div>
+        <div class="my-6 flex items-center justify-between">
+            <div class="w-full rounded-lg border-2 border-gray2 p-2 h-[380px] shadow-md mr-2">
+                <div class="flex items-center">
                     <div><img class="w-10 h-10" src="{{ URL::asset('/images/pratique.png') }}" alt=""></div>
                     <div class="font-bold">Apprentissage pratique</div>
-                    <div class="text-justify h-auto">Nous mettons l'accent sur l'apprentissage pratique et l'acquisition de
-                        compétences concrètes. Nous croyons fermement que l'apprentissage par la pratique est la clé pour
-                        développer des compétences durables. Nos formateurs expérimentés vous guideront tout au long du
-                        processus, en vous offrant des exercices pratiques et des projets concrets pour renforcer votre
-                        compréhension et votre expertise.</div>
                 </div>
+                <div class="text-justify h-auto">Nous mettons l'accent sur l'apprentissage pratique et l'acquisition de
+                    compétences concrètes. Nous croyons fermement que l'apprentissage par la pratique est la clé pour
+                    développer des compétences durables. Nos formateurs expérimentés vous guideront tout au long du
+                    processus, en vous offrant des exercices pratiques et des projets concrets pour renforcer votre
+                    compréhension et votre expertise.</div>
             </div>
-
-            <div class="grid">
-                <div class="max-w-full rounded-lg border-2 border-gray2 p-2 h-[420px] shadow-md">
+            <div class="w-full rounded-lg border-2 border-gray2 p-2 h-[380px] shadow-md mr-2">
+                <div class="flex items-center">
                     <div><img class="w-10 h-10" src="{{ URL::asset('/images/special.png') }}" alt=""></div>
                     <div class="font-bold">Formations spécialisées</div>
-                    <div class="text-justify h-auto">Nous proposons des formations spécialisées dans divers domaines pour
-                        vous aider à acquérir de
-                        nouvelles compétences et à vous perfectionner dans votre carrière. Que vous souhaitiez vous
-                        spécialiser en développement informatique, en langues étrangères, en gestion de projet ou dans
-                        d'autres secteurs passionnants, notre centre de formation est là pour vous accompagner.</div>
                 </div>
+                <div class="text-justify h-auto">Nous proposons des formations spécialisées dans divers domaines pour
+                    vous aider à acquérir de
+                    nouvelles compétences et à vous perfectionner dans votre carrière. Que vous souhaitiez vous
+                    spécialiser en développement informatique, en langues étrangères, en gestion de projet ou dans
+                    d'autres secteurs passionnants, notre centre de formation est là pour vous accompagner.</div>
             </div>
-            <div class="grid">
-                <div class="max-w-full rounded-lg border-2 border-gray2 p-2 h-[330px] shadow-md">
+            <div class="w-full rounded-lg border-2 border-gray2 p-2 h-[380px] shadow-md mr-2">
+                <div class="flex items-center">
                     <div><img class="w-10 h-10" src="{{ URL::asset('/images/experience.png') }}" alt=""></div>
                     <div class="font-bold">Formateurs expérimentés</div>
-                    <div class="text-justify h-auto">Nous avons une équipe de formateurs expérimentés et compétents qui sont
-                        passionnés par leur domaine d'expertise. Leur expérience pratique et leur connaissance approfondie
-                        du sujet garantissent un apprentissage de qualité et une compréhension approfondie des concepts.
-                    </div>
+                </div>
+                <div class="text-justify h-auto">Nous avons une équipe de formateurs expérimentés et compétents qui sont
+                    passionnés par leur domaine d'expertise. Leur expérience pratique et leur connaissance approfondie
+                    du sujet garantissent un apprentissage de qualité et une compréhension approfondie des concepts.
                 </div>
             </div>
-            <div class="grid gap-0">
-                <div class="grid">
-                    <div class="max-w-full rounded-lg border-2 border-gray2 p-2 h-[330px] shadow-md">
-                        <div><img class="w-10 h-10" src="{{ URL::asset('/images/adapt.png') }}" alt=""></div>
-                        <div class="font-bold">Adapté à vos besoins</div>
-                        <div class="text-justify h-auto">Nos programmes de formation sont conçus pour répondre aux besoins
-                            spécifiques de chaque apprenant. Nous comprenons que chaque individu a des objectifs uniques,
-                            c'est pourquoi nos formations sont personnalisées pour vous aider à atteindre vos objectifs
-                            professionnels.</div>
-                    </div>
+            <div class="w-full rounded-lg border-2 border-gray2 p-2 h-[380px] shadow-md">
+                <div class="flex items-center">
+                    <div><img class="w-10 h-10" src="{{ URL::asset('/images/adapt.png') }}" alt=""></div>
+                    <div class="font-bold">Adapté à vos besoins</div>
                 </div>
+                <div class="text-justify h-auto">Nos programmes de formation sont conçus pour répondre aux besoins
+                    spécifiques de chaque apprenant. Nous comprenons que chaque individu a des objectifs uniques,
+                    c'est pourquoi nos formations sont personnalisées pour vous aider à atteindre vos objectifs
+                    professionnels.</div>
             </div>
         </div>
 
@@ -136,13 +105,13 @@
                 <li class="mt-2">
                     <i class="fa-solid fa-at"></i> contact.megadev@gmail.com
                 </li>
-                <li  class="mt-2">
+                <li class="mt-2">
                     <i class="fa-solid fa-phone-volume"></i> 05 37 81 12 68
                 </li>
-                <li  class="mt-2">
+                <li class="mt-2">
                     <i class="fa-solid fa-location-dot"></i> N° 14, Secteur 11 Extension,Hay Salam
                 </li>
-                <li  class="mt-2">
+                <li class="mt-2">
                     <i class="fa-solid fa-city"></i> Salé 11003 Maroc
                 </li>
             </ul>
@@ -150,11 +119,10 @@
             <ul class="text-center sm:text-start ml-28">
                 <h1 class="mb-1 font-semibold">Pages</h1>
                 <li class="mt-2">
-                    <a
-                        class="text-white hover:text-gray1 duration-300
+                    <a class="text-white hover:text-gray1 duration-300
                      text-md cursor-pointer leading-6"
                         href='/'>
-                    Accueil
+                        Accueil
                     </a>
                 </li>
                 <li class="mt-2">
--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -2,5 +2,5 @@
     <div class="mr-2">
         <img class="w-10 h-9" src="{{ URL::asset('/images/logopen.png') }}" alt="">
     </div>
-    <div class="font-extrabold text-2xl mt-1"><a href="/">Megadev</a></div>
+    <div class="font-extrabold text-2xl mt-1"><a href="/">MegaDev</a></div>
 </div>
