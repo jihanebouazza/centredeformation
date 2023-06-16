30de093a7eb46898ee09f944fb5fd8d36d27fdd1
oussama_rezkellah <orezkellah@gmail.com>
2023-06-16 01:23:21 +0100

    invite

--- resources/views/emails/invite.blade.php
+++ resources/views/emails/invite.blade.php
@@ -1,1 +1,18 @@
-<p>Click the following link to reset your password</p>
+<p>Bonjour {{$first_name}}
+    
+    Suite a l etude de votre candidature, nous avons le plaisir de vous inviter a passer un entretien physique dans nos locaux.
+        
+    Nous vous invitons a confirmer votre presence, par la simple reponse a cette e-mail.
+    
+    Nous restons a votre disposition quant aux differentes questions que vous pourriez rencontrer, et nous vous souhaitons une bonne reussite dans cette serie d entretien.
+    
+    Nous avons hate de vous voir.
+    
+    Bien cordialement,
+    
+    Le service des ressources humaines,
+    
+    Oussama Rezkellah
+    directeur administratif,
+    MegaDev@gmail.com";
+</p>
