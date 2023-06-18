df8c3e55fb4a79175cd57eab5436694edcd248cb
jiiiiiiiiiiiiiiiiiiiiiiii <110284532+jiiiiiiiiiiiiiiiiiiiiiiii@users.noreply.github.com>
2023-06-18 12:13:39 +0100

    interface mot de passe et emploi formateur/etudiant

--- resources/views/components/sidebar-etudiant.blade.php
+++ resources/views/components/sidebar-etudiant.blade.php
@@ -1,41 +1,59 @@
 <div class="fixed top-0 left-0 bottom-0 w-[280px] border-r">
-  <div class="mt-[8%] ml-[4%]">
-      <x-logo />
-  </div>
-  <div style="{{ $active === 1 ? 'background-color: rgba(201, 214, 223, 0.4)' : '' }}"
-      class="mt-8 py-2 p-1 {{ $active === 1 ? 'mx-[4%] text-black2 border-l-4 pl-2 rounded-lg border-r-0 border-t-0 border-b-0 border-4 border-black2' : 'ml-[6%] ' }} hover:mx-[4%] hover:text-black2 hover:border-l-4 hover:pl-2 hover:rounded-lg hover:border-r-0 hover:border-t-0 hover:border-b-0 hover:border-4 hover:border-black2 transition-all duration-200">
-      <a href="/dashboardE">
-          <div class="flex items-center">
-              <div class="mr-2 text-[17px]"><i class="fa-solid fa-chart-line"></i></div>
-              <div class="font-semibold text-[18px]">Dashboard</div>
-          </div>
-      </a>
-  </div>
-  <div style="{{ $active === 2 ? 'background-color: rgba(201, 214, 223, 0.4)' : '' }}"
-      class="mt-4 py-2 p-1 {{ $active === 2 ? 'mx-[4%] text-black2 border-l-4 pl-2 rounded-lg border-r-0 border-t-0 border-b-0 border-4 border-black2' : 'ml-[6%] ' }} hover:mx-[4%] hover:text-black2 hover:border-l-4 hover:pl-2 hover:rounded-lg hover:border-r-0 hover:border-t-0 hover:border-b-0 hover:border-4 hover:border-black2 transition-all duration-200">
-      <a href="/profileE">
-          <div class="flex items-center">
-              <div class="mr-2 text-[17px]"><i class="fa-solid fa-chalkboard-user"></i></div>
-              <div class="font-semibold text-[18px]">Profile</div>
-          </div>
-      </a>
-  </div>
-  <div style="{{ $active === 3 ? 'background-color: rgba(201, 214, 223, 0.4)' : '' }}"
-      class="mt-4 py-2 p-1 {{ $active === 3 ? 'mx-[4%] text-black2 border-l-4 pl-2 rounded-lg border-r-0 border-t-0 border-b-0 border-4 border-black2' : 'ml-[6%] ' }} hover:mx-[4%] hover:text-black2 hover:border-l-4 hover:pl-2 hover:rounded-lg hover:border-r-0 hover:border-t-0 hover:border-b-0 hover:border-4 hover:border-black2 transition-all duration-200">
-      <a href="/opinions">
-          <div class="flex items-center">
-              <div class="mr-2 text-[17px]"><i class="fa-regular fa-comments"></i></div>
-              <div class="font-semibold text-[18px]">Opinions</div>
-          </div>
-      </a>
-  </div>
-  <div style="{{ $active === 4 ? 'background-color: rgba(201, 214, 223, 0.4)' : '' }}"
-      class="mt-4 py-2 p-1 {{ $active === 4 ? 'mx-[4%] text-black2 border-l-4 pl-2 rounded-lg border-r-0 border-t-0 border-b-0 border-4 border-black2' : 'ml-[6%] ' }} hover:mx-[4%] hover:text-black2 hover:border-l-4 hover:pl-2 hover:rounded-lg hover:border-r-0 hover:border-t-0 hover:border-b-0 hover:border-4 hover:border-black2 transition-all duration-200">
-      <a href="/history">
-          <div class="flex items-center">
-              <div class="mr-2 text-[17px]"><i class="fa-solid fa-clock-rotate-left"></i></div>
-              <div class="font-semibold text-[18px]">Historique</div>
-          </div>
-      </a>
-  </div>
+    <div class="mt-[8%] ml-[4%]">
+        <x-logo />
+    </div>
+    <div style="{{ $active === 1 ? 'background-color: rgba(201, 214, 223, 0.4)' : '' }}"
+        class="mt-8 py-2 p-1 {{ $active === 1 ? 'mx-[4%] text-black2 border-l-4 pl-2 rounded-lg border-r-0 border-t-0 border-b-0 border-4 border-black2' : 'ml-[6%] ' }} hover:mx-[4%] hover:text-black2 hover:border-l-4 hover:pl-2 hover:rounded-lg hover:border-r-0 hover:border-t-0 hover:border-b-0 hover:border-4 hover:border-black2 transition-all duration-200">
+        <a href="/dashboardE">
+            <div class="flex items-center">
+                <div class="mr-2 text-[17px]"><i class="fa-solid fa-chart-line"></i></div>
+                <div class="font-semibold text-[18px]">Dashboard</div>
+            </div>
+        </a>
+    </div>
+    <div style="{{ $active === 2 ? 'background-color: rgba(201, 214, 223, 0.4)' : '' }}"
+        class="mt-4 py-2 p-1 {{ $active === 2 ? 'mx-[4%] text-black2 border-l-4 pl-2 rounded-lg border-r-0 border-t-0 border-b-0 border-4 border-black2' : 'ml-[6%] ' }} hover:mx-[4%] hover:text-black2 hover:border-l-4 hover:pl-2 hover:rounded-lg hover:border-r-0 hover:border-t-0 hover:border-b-0 hover:border-4 hover:border-black2 transition-all duration-200">
+        <a href="/profileE">
+            <div class="flex items-center">
+                <div class="mr-2 text-[17px]"><i class="fa-solid fa-chalkboard-user"></i></div>
+                <div class="font-semibold text-[18px]">Profile</div>
+            </div>
+        </a>
+    </div>
+    <div style="{{ $active === 3 ? 'background-color: rgba(201, 214, 223, 0.4)' : '' }}"
+        class="mt-4 py-2 p-1 {{ $active === 3 ? 'mx-[4%] text-black2 border-l-4 pl-2 rounded-lg border-r-0 border-t-0 border-b-0 border-4 border-black2' : 'ml-[6%] ' }} hover:mx-[4%] hover:text-black2 hover:border-l-4 hover:pl-2 hover:rounded-lg hover:border-r-0 hover:border-t-0 hover:border-b-0 hover:border-4 hover:border-black2 transition-all duration-200">
+        <a href="/opinions">
+            <div class="flex items-center">
+                <div class="mr-2 text-[17px]"><i class="fa-regular fa-comments"></i></div>
+                <div class="font-semibold text-[18px]">Opinions</div>
+            </div>
+        </a>
+    </div>
+    <div style="{{ $active === 4 ? 'background-color: rgba(201, 214, 223, 0.4)' : '' }}"
+        class="mt-4 py-2 p-1 {{ $active === 4 ? 'mx-[4%] text-black2 border-l-4 pl-2 rounded-lg border-r-0 border-t-0 border-b-0 border-4 border-black2' : 'ml-[6%] ' }} hover:mx-[4%] hover:text-black2 hover:border-l-4 hover:pl-2 hover:rounded-lg hover:border-r-0 hover:border-t-0 hover:border-b-0 hover:border-4 hover:border-black2 transition-all duration-200">
+        <a href="/history">
+            <div class="flex items-center">
+                <div class="mr-2 text-[17px]"><i class="fa-solid fa-clock-rotate-left"></i></div>
+                <div class="font-semibold text-[18px]">Historique</div>
+            </div>
+        </a>
+    </div>
+    <div style="{{ $active === 5 ? 'background-color: rgba(201, 214, 223, 0.4)' : '' }}"
+        class="mt-4 py-2 p-1 {{ $active === 5 ? 'mx-[4%] text-black2 border-l-4 pl-2 rounded-lg border-r-0 border-t-0 border-b-0 border-4 border-black2' : 'ml-[6%] ' }} hover:mx-[4%] hover:text-black2 hover:border-l-4 hover:pl-2 hover:rounded-lg hover:border-r-0 hover:border-t-0 hover:border-b-0 hover:border-4 hover:border-black2 transition-all duration-200">
+        <a href="/emploiE">
+            <div class="flex items-center">
+                <div class="mr-2 text-[17px]"><i class="fa-solid fa-calendar-days"></i></div>
+                <div class="font-semibold text-[18px]">Emploi du temps</div>
+            </div>
+        </a>
+    </div>
+    <div style="{{ $active === 6 ? 'background-color: rgba(201, 214, 223, 0.4)' : '' }}"
+        class="mt-4 py-2 p-1 {{ $active === 6 ? 'mx-[4%] text-black2 border-l-4 pl-2 rounded-lg border-r-0 border-t-0 border-b-0 border-4 border-black2' : 'ml-[6%] ' }} hover:mx-[4%] hover:text-black2 hover:border-l-4 hover:pl-2 hover:rounded-lg hover:border-r-0 hover:border-t-0 hover:border-b-0 hover:border-4 hover:border-black2 transition-all duration-200">
+        <a href="/passwordE">
+            <div class="flex items-center">
+                <div class="mr-2 text-[17px]"><i class="fa-solid fa-lock"></i></div>
+                <div class="font-semibold text-[18px]">Mot de passe</div>
+            </div>
+        </a>
+    </div>
 </div>
--- resources/views/formateur/dashboard.blade.php
+++ resources/views/formateur/dashboard.blade.php
@@ -1,5 +1,87 @@
 @extends('layout')
 
 @section('content')
-
+    <div id="header" class="sticky top-0 left-0 right-0 flex items-center justify-between bg-black1 py-8 px-6 z-50">
+        <div>
+            <x-logo id="logo" class="text-white" />
+        </div>
+        <div class="flex items-center justify-between text-white w-[30%] pr-8" id="words">
+            <div class="text-md font-semibold mr-2"><a href="/dashboardF">Emploi du temps</a></div>
+            <div class="text-md font-semibold"><a href="/passwordF">Mot de passe</a></div>
+        </div>
+    </div>
+    <div>
+        <div class="w-full overflow-x-scroll">
+            <table class="w-full text-left text-base font-light">
+                <thead class="border-b font-medium">
+                    <tr>
+                        <th scope="col" class="px-6 py-4"></th>
+                        <th scope="col" class="px-6 py-4">Lundi</th>
+                        <th scope="col" class="px-6 py-4">Mardi</th>
+                        <th scope="col" class="px-6 py-4">Mercredi</th>
+                        <th scope="col" class="px-6 py-4">Jeudi</th>
+                        <th scope="col" class="px-6 py-4">Vendredi</th>
+                        <th scope="col" class="px-6 py-4">Samedi</th>
+                        <th scope="col" class="px-6 py-4">Dimanche</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="border-b transition duration-300 ease-in-out hover:bg-[#F1F6F9]">
+                        <td class="whitespace-nowrap px-6 py-4">17:00 - 18:30</td>
+                        <td class="whitespace-nowrap px-6 py-4" data-jour="lundi" data-heure="17:00 - 18:30"></td>
+                        <td class="whitespace-nowrap px-6 py-4" data-jour="mardi" data-heure="17:00 - 18:30"></td>
+                        <td class="whitespace-nowrap px-6 py-4" data-jour="mercredi" data-heure="17:00 - 18:30">
+                        </td>
+                        <td class="whitespace-nowrap px-6 py-4" data-jour="jeudi" data-heure="17:00 - 18:30"></td>
+                        <td class="whitespace-nowrap px-6 py-4" data-jour="vendredi" data-heure="17:00 - 18:30">
+                        </td>
+                        <td class="whitespace-nowrap px-6 py-4" data-jour="samedi" data-heure="17:00 - 18:30"></td>
+                        <td class="whitespace-nowrap px-6 py-4" data-jour="dimanche" data-heure="17:00 - 18:30">
+                        </td>
+                    </tr>
+                    <tr class="border-b transition duration-300 ease-in-out hover:bg-[#F1F6F9]">
+                        <td class="whitespace-nowrap px-6 py-4">18:30 - 20:00</td>
+                        <td class="whitespace-nowrap px-6 py-4" data-jour="lundi" data-heure="18:30 - 20:00"></td>
+                        <td class="whitespace-nowrap px-6 py-4" data-jour="mardi" data-heure="18:30 - 20:00"></td>
+                        <td class="whitespace-nowrap px-6 py-4" data-jour="mercredi" data-heure="18:30 - 20:00">
+                        </td>
+                        <td class="whitespace-nowrap px-6 py-4" data-jour="jeudi" data-heure="18:30 - 20:00"></td>
+                        <td class="whitespace-nowrap px-6 py-4" data-jour="vendredi" data-heure="18:30 - 20:00">
+                        </td>
+                        <td class="whitespace-nowrap px-6 py-4" data-jour="samedi" data-heure="18:30 - 20:00"></td>
+                        <td class="whitespace-nowrap px-6 py-4" data-jour="dimanche" data-heure="18:30 - 20:00">
+                        </td>
+                    </tr>
+                    <tr class="border-b transition duration-300 ease-in-out hover:bg-[#F1F6F9]">
+                        <td class="whitespace-nowrap px-6 py-4">20:00 - 21:30</td>
+                        <td class="whitespace-nowrap px-6 py-4" data-jour="lundi" data-heure="20:00 - 21:30"></td>
+                        <td class="whitespace-nowrap px-6 py-4" data-jour="mardi" data-heure="20:00 - 21:30"></td>
+                        <td class="whitespace-nowrap px-6 py-4" data-jour="mercredi" data-heure="20:00 - 21:30">
+                        </td>
+                        <td class="whitespace-nowrap px-6 py-4" data-jour="jeudi" data-heure="20:00 - 21:30"></td>
+                        <td class="whitespace-nowrap px-6 py-4" data-jour="vendredi" data-heure="20:00 - 21:30">
+                        </td>
+                        <td class="whitespace-nowrap px-6 py-4" data-jour="samedi" data-heure="20:00 - 21:30"></td>
+                        <td class="whitespace-nowrap px-6 py-4" data-jour="dimanche" data-heure="20:00 - 21:30">
+                        </td>
+                    </tr>
+                    <tr class="border-b transition duration-300 ease-in-out hover:bg-[#F1F6F9]">
+                        <td class="whitespace-nowrap px-6 py-4">21:30 - 23:00</td>
+                        <td class="whitespace-nowrap px-6 py-4" data-jour="lundi" data-heure="21:30 - 23:00"></td>
+                        <td class="whitespace-nowrap px-6 py-4" data-jour="mardi" data-heure="21:30 - 23:00"></td>
+                        <td class="whitespace-nowrap px-6 py-4" data-jour="mercredi" data-heure="21:30 - 23:00">
+                        </td>
+                        <td class="whitespace-nowrap px-6 py-4" data-jour="jeudi" data-heure="21:30 - 23:00">
+                        </td>
+                        <td class="whitespace-nowrap px-6 py-4" data-jour="vendredi" data-heure="21:30 - 23:00">
+                        </td>
+                        <td class="whitespace-nowrap px-6 py-4" data-jour="samedi" data-heure="21:30 - 23:00">
+                        </td>
+                        <td class="whitespace-nowrap px-6 py-4" data-jour="dimanche" data-heure="21:30 - 23:00">
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 @endsection
